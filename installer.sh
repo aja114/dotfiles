522f73827d26db3d9f55ba85462cde44156cd8c9
AlexAC <alexandreabouchahine@gmail.com>
2022-06-18 16:36:49 +0200

    add p10k installer

--- installer.sh
+++ installer.sh
@@ -3,8 +3,8 @@ if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fssl https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-if [ ! -d ~/.oh-my-zsh ]; then
-    sh -c "$(curl -fssl https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
 
