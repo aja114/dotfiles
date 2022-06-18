c4c081614fc188d009d832d7d8bdbfec5f173f7f
AlexAC <alexandreabouchahine@gmail.com>
2022-06-18 15:55:11 +0200

    new installer

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 # Install oh-my-zsh
-if [[ ! -d ~/.oh-my-zsh ]]; then:
+if [[ ! -d ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi;
+fi
 
 # Add the config 
-cat "source ~/dotfiles/.zshrc" > ~/.zshrc
+echo "source $HOME/dotfiles/.zshrc" > $HOME/.zshrc
 
 
