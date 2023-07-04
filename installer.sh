04b7a8842f5f668687fce6290f6f7ca98d6e3223
AlexAC <alexandreabouchahine@gmail.com>
2023-07-04 21:57:57 +0200

    add some zsh plugins

--- installer.sh
+++ installer.sh
@@ -1,12 +1,24 @@
-# Install oh-my-zsh
+#!/bin/bash
+set -ex
+
+ZSH_HOME=${ZSH_CUSTOM:-$HOME/.oh-my-zsh}
+
+# Install oh-my-zsh if it does not exist
 if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-if [ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# Install p10k if it does not exist
+if [ ! -d ${ZSH_HOME}/custom/themes/powerlevel10k ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_HOME}/custom/themes/powerlevel10k
 fi
+
+# install zsh plugins
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_HOME}/plugins/zsh-autocomplete
 
 
 # Add all the configs files to $home
 cp configs/* $HOME/
+
+# Add the script files to $home
+cp -r scripts $HOME/scripts
